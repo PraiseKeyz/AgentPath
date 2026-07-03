30a6d8c132da8e6e15ba801d95ed515daf4d0a89
praisekeyz <praiseoluwatobilobaadebayo@gmail.com>
2026-07-03 09:56:45 +0100

    chore: remove upkora leftover files

--- frontend/src/constants/cohort.ts
+++
@@ -1,1 +0,0 @@
-export const nextCohortStartDate = new Date('2026-07-15T09:00:00')
--- frontend/src/constants/layout.ts
+++
@@ -1,7 +0,0 @@
-export const appContainerClass = 'mx-auto w-full max-w-[1280px] px-4 md:px-16'
-
-export const appContentWidthClass = 'mx-auto w-full max-w-[1280px]'
-
-export const appContainerNarrowClass = 'mx-auto w-full max-w-3xl px-4 md:px-16'
-
-export const appContainerProseClass = 'mx-auto w-full max-w-[48rem] px-4 md:px-16'
